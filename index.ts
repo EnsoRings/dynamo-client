98579e5e5c95ed23a32813a1b3690ee9df7595cf
Romina Killpack <romina.killpack@sunshinegardens.org>
2021-11-17 14:16:30 -0700

    rework of dynamongo to work in vercel deployments
    - pin node to lts
    - add "table scoped" db client class
    - fix getByKey bug (wrapped response in `{ Item: {} }`)
    - allow constructor to set AWS credentials
    - use ttl-compatible timestamp for created and updated fields

--- index.ts
+++ index.ts
@@ -1,148 +1,171 @@
 const AWS = require('aws-sdk');
-
 interface DynamongoOptions {
-	isVerbose?: boolean,
-	timestampsEnabled?: boolean,
-	timestampCreatedField?: string,
-	timestampUpdatedField?: string
+  isVerbose?: boolean,
+  timestampsEnabled?: boolean,
+  timestampCreatedField?: string,
+  timestampUpdatedField?: string,
+  accessKeyId?: string,
+  secretAccessKey?: string,
 }
-
 interface DynamongoExpression {
-	data: string,
-	values: object,
-	names: object
+  data: string,
+  values: object,
+  names: object
 }
-
-class Dynamongo {
-	
-	/* Optional Singleton Usage */
-	private static _Instance = null;
-
-	public static GetInstance() {
-		if (Dynamongo._Instance == null) {
-			// @ts-ignore
-			Dynamongo._Instance = new Dynamongo(...arguments);
-		}
-
-		return Dynamongo._Instance;
-	}
-
-	private _connection = null;
-	private _isVerbose = false;
-	private _timestampsEnabled = false;
-	private _timestampCreatedField = 'createdAt';
-	private _timestampUpdatedField = 'updatedAt';
-
-	constructor(region, options: DynamongoOptions) {
-		options = options || {};
-
-		if (region) {
-			AWS.config.update({ region });
-		}
-
-		this._connection = new AWS.DynamoDB.DocumentClient();
-		this._isVerbose = options.isVerbose;
-		this._timestampsEnabled = options.timestampsEnabled;
-		this._timestampCreatedField = options.timestampCreatedField;
-		this._timestampUpdatedField = options.timestampUpdatedField;
-	}
-
-	public getConnection() {
-		return this._connection;
-	}
-
-	private _log(...args): void {
-		this._isVerbose && console.log('Dynamongo: ', ...args);
-	}
-
-	async getWhere(table: string, where: object) {
-		const expression = this._generateExpression(where, 'AND');
-
-		const params = {
-			TableName: table,
-			FilterExpression: expression.data,
-			ExpressionAttributeValues: expression.values,
-			ExpressionAttributeNames: expression.names
-		};
-
-		this._log('getWhere()', params);
-		return await this._connection.scan(params).promise();
-	}
-
-	_generateExpression(data: object, expressionSeparator: string): DynamongoExpression  {
-		const expressionData = [];
-		const expressionAttributeValues = {};
-		const expressionAttributeNames = {};
-
-		Object.keys(data).forEach((key, index) => {
-			key = key.replace(/[^A-Za-z]/gi, '');
-
-			expressionData.push(`#${index} = :val${index}`);
-			expressionAttributeValues[`:val${index}`] = data[key];
-			expressionAttributeNames[`#${index}`] = key;
-		});
-
-		return {
-			data: expressionData.join(` ${expressionSeparator} `),
-			values: expressionAttributeValues,
-			names: expressionAttributeNames
-		}
-	}
-
-	async insert(table: string, data: object) : Promise<object> {
-		const params = {
-			TableName: table,
-			Item: data
-		};
-
-		if (this._timestampsEnabled) {
-			params.Item[this._timestampCreatedField] = Date.now();
-		}
-
-		this._log('insert()', params);
-		return await this._connection.put(params).promise();
-	}
-
-	async getByKey(table: string, keys: object): Promise<object> {
-		const params = {
-			TableName: table,
-			Key: keys
-		};
-
-		this._log('getByKey()', params);
-		return await this._connection.get(params).promise();
-	}
-
-	async updateByKey(table: string, keys: object, data: object): Promise<object>{
-		if (this._timestampsEnabled) {
-			data[this._timestampUpdatedField] = Date.now();
-		}
-
-		const expression = this._generateExpression(data, ',');
-		
-		const params = {
-			TableName: table,
-			Key: keys,
-			UpdateExpression: `SET ${expression.data}`,
-			ExpressionAttributeValues: expression.values,
-			ExpressionAttributeNames: expression.names,
-			ReturnValues: "UPDATED_NEW"
-		};
-
-		this._log('updateByKey()', params);
-		return await this._connection.update(params).promise();
-	}
-
-	async deleteByKey(table: string, keys: object): Promise<object>{
-		const params = {
-			TableName: table,
-			Key: keys
-		};
-
-		this._log('deleteByKey()', params);
-		return await this._connection.delete(params).promise();
-	}
-
+/**
+ */
+class DynamoDB {
+  private _connection = null;
+  private _isVerbose = false;
+  private _timestampsEnabled = false;
+  private _timestampCreatedField = 'createdAt';
+  private _timestampUpdatedField = 'updatedAt';
+  constructor(region, options: DynamongoOptions) {
+    options = options || {};
+    if (region && options.accessKeyId && options.secretAccessKey) {
+      // optionally override the environment
+      AWS.config.update({
+        accessKeyId: options.accessKeyId,
+        secretAccessKey: options.secretAccessKey,
+        region,
+      })
+    } else if (region) {
+      AWS.config.update({ region });
+    }
+    this._connection = new AWS.DynamoDB.DocumentClient();
+    this._isVerbose = options.isVerbose;
+    this._timestampsEnabled = options.timestampsEnabled;
+    if (options.timestampCreatedField) {
+      this._timestampCreatedField = options.timestampCreatedField;
+    }
+    if (options.timestampUpdatedField) {
+      this._timestampUpdatedField = options.timestampUpdatedField;
+    }
+  }
+  private _log(...args): void {
+    this._isVerbose && console.log('Dynamongo: ', ...args);
+  }
+  /**
+  */
+  _generateExpression(data: object, expressionSeparator: string): DynamongoExpression  {
+    const expressionData = [];
+    const expressionAttributeValues = {};
+    const expressionAttributeNames = {};
+    Object.keys(data).forEach((key, index) => {
+      key = key.replace(/[^A-Za-z]/gi, '');
+      expressionData.push(`#${index} = :val${index}`);
+      expressionAttributeValues[`:val${index}`] = data[key];
+      expressionAttributeNames[`#${index}`] = key;
+    });
+    return {
+      data: expressionData.join(` ${expressionSeparator} `),
+      values: expressionAttributeValues,
+      names: expressionAttributeNames
+    }
+  }
+  /**
+   * Date.now() gives a valid unix time in ms, but dynamodb ttl only works with a
+   * resolution of *seconds* since unix epoch.
+   */
+   public unixEpoch() {
+    return Math.trunc(Date.now() / 1000);
+  }
+  /**
+  */
+  public getConnection() {
+    return this._connection;
+  }
+  /**
+  */
+  public async getWhere(table: string, where: object) {
+    const expression = this._generateExpression(where, 'AND');
+    const params = {
+      TableName: table,
+      FilterExpression: expression.data,
+      ExpressionAttributeValues: expression.values,
+      ExpressionAttributeNames: expression.names
+    };
+    this._log('getWhere()', params);
+    return await this._connection.scan(params).promise();
+  }
+  /**
+  */
+  public async insert(table: string, data: object) : Promise<object> {
+    const params = {
+      TableName: table,
+      Item: data
+    };
+    if (this._timestampsEnabled) {
+      params.Item[this._timestampCreatedField] = this.unixEpoch();
+    }
+    this._log('insert()', params);
+    return await this._connection.put(params).promise();
+  }
+  /**
+  */
+  public async getByKey(table: string, keys: object): Promise<object> {
+    const params = {
+      TableName: table,
+      Key: keys
+    };
+    this._log('getByKey()', params);
+    const response = await this._connection.get(params).promise();
+    return (response && response.Item) ? response.Item : response
+  }
+  /**
+  */
+  public async updateByKey(table: string, keys: object, data: object): Promise<object>{
+    if (this._timestampsEnabled) {
+      data[this._timestampUpdatedField] = this.unixEpoch();
+    }
+    const expression = this._generateExpression(data, ',');
+    const params = {
+      TableName: table,
+      Key: keys,
+      UpdateExpression: `SET ${expression.data}`,
+      ExpressionAttributeValues: expression.values,
+      ExpressionAttributeNames: expression.names,
+      ReturnValues: "UPDATED_NEW"
+    };
+    this._log('updateByKey()', params);
+    return await this._connection.update(params).promise();
+  }
+  /**
+  */
+  public async deleteByKey(table: string, keys: object): Promise<object>{
+    const params = {
+      TableName: table,
+      Key: keys
+    };
+    this._log('deleteByKey()', params);
+    return await this._connection.delete(params).promise();
+  }
 }
-
-module.exports = Dynamongo;
+/**
+ * table-scobed DB instance
+ * @param {string} region - dc region
+ * @param {string} table - table to scope this instance to
+ */
+class DynamoTable extends DynamoDB {
+  public table = null
+  constructor(region, table, options: DynamongoOptions) {
+    super(region, options)
+    this.table = table
+  }
+  public getByKey(keys) {
+    return super.getByKey(this.table, keys)
+  }
+  public updateByKey(keys, data) {
+    return super.updateByKey(this.table, keys, data)
+  }
+  public deleteByKey(keys) {
+    return super.deleteByKey(this.table, keys)
+  }
+  public getWhere(data) {
+    return super.getWhere(this.table, data)
+  }
+  public insert(data) {
+    return super.insert(this.table, data)
+  }
+}
+module.exports = { DynamoDB, DynamoTable };
--- readme.md
+++ readme.md
@@ -1,43 +1,6 @@
-[![Contributors][contributors-shield]][contributors-url]
-[![Forks][forks-shield]][forks-url]
-[![Stargazers][stars-shield]][stars-url]
-[![Issues][issues-shield]][issues-url]
-[![Apache-2.0 License][license-shield]][license-url]
-[![LinkedIn][linkedin-shield]][linkedin-url]
+# enso-rings/dynamo-client
 
-![npm version](https://badge.fury.io/js/dynamongo.svg)
-![npm dependencies](https://status.david-dm.org/gh/kevinvr/dynamongo.svg) [![Maintainability](https://api.codeclimate.com/v1/badges/82afbc1cbbfdc6094302/maintainability)](https://codeclimate.com/github/KevinVR/dynamongo/maintainability)
-
-
-<br />
-<p align="center">
-  <h1 align="center">DynaMongo</h1>
-  <h2 align="center">
-    A simple library for easy querying of DynamoDB using MongoDB-style object queries
-  </h2>
-  <h3 align="center">
-    Forget about expression filters and conditions.
-  </h3>
-</p>
-
-## About The Project
-
-Coming from MongoDB and moving towards DynamoDB, there are some abstractions that could make our lives easier. For instance, with DynamoDB, expression clauses have to be manually written as strings of instructions, while MongoDB allows for simple objects.
-
-That's where this library comes in. It should mimick the basic MongoDB style of queries, not requiring any hand-written or generated conditions. Instead, pass in an object, Dynamongo will take care of sending it in the correct way to DynamoDB. 
-
-_Note, this does not support any of the advanced MongoDB querying, instead, this simply allows you to use objects directly instead of having to write specific expressions._
-
-Here's why:
-* Stop spending time on writing the same database boilerplate for handling dynamic DynamoDB expressions and filters
-* Use a library which takes care of this for you instead
-* Focus your time on actually writing your application instead of database utilities
-
-### Built With
-
-This section should list any major frameworks that you built your project using. Leave any add-ons/plugins for the acknowledgements section. Here are a few examples.
-* [TypeScript](http://typescriptlang.org)
-* [AWS-SDK](https://www.npmjs.com/package/aws-sdk)
+a simplified client interface for dynamodb.
 
 ## Getting Started
 
@@ -45,36 +8,54 @@ To get a local copy up and running follow these simple example steps.
 
 ### Prerequisites
 
-Install the Dynamongo package into your project
-  ```sh
-  npm install dynamongo
-  ```
+Install the dynamo-client package into your project
+
+```sh
+npm install @enso-rings/dynamo-client
+```
 
 ## Usage
 
-Get a Dynamongo instance, either through the singleton helper, or by instantiating it manually. Enter the region and optionally an object with supported options.
+Get a dynamo-client instance: enter the region and optionally an object with
+supported options. `DynamoTable` supports all of the same methods, but provides
+a convenient shorthand when making repeated changes to the same table.
+
 ```js
-const db = Dynamongo.GetInstance('eu-west-1', {
+const { DynamoDB, DynamoTable } = require('@enso-rings/dynamo-client')
+const db = new DynamoDB('eu-west-1', {
 	isVerbose: true,
 	timestampsEnabled: true,
 	timestampCreatedField: 'createdAt',
 	timestampUpdatedField: 'updatedAt'
 });
+// "table scoped" db client
+const table = new DynamoTable('ap-east-1', 'my-table-name', {
+	isVerbose: true,
+	timestampsEnabled: true,
+	timestampCreatedField: 'createdAt',
+	timestampUpdatedField: 'updatedAt'
+})
 ```
 
 Now you are ready to use this instance for querying the database.
 
 ### getByKey(table, keys)
+
 ```js
 const data = await db.getByKey('my-db-table', {customId: 'abc-123'});
 ```
 
 ### updateByKey(table, keys, data)
+
 ```js
-const result = await db.updateByKey('my-db-table', {customId: 'abc-123'}, {newData: '123'});
+const result = await db.updateByKey('my-db-table',
+  {customId: 'abc-123'},
+  {newData: '123'}
+);
 ```
 
 ### deleteByKey(table, keys)
+
 ```js
 const result = await db.deleteByKey('my-db-table', {customId: 'abc-123'});
 ```
@@ -82,24 +63,33 @@ const result = await db.deleteByKey('my-db-table', {customId: 'abc-123'});
 ### getWhere(table, data)
 
 ```js
-const data = await db.getWhere('my-db-table', {firstName: 'Kevin', lastName: 'Van Ryckegem'});
+const data = await db.getWhere('my-db-table', {
+  firstName: 'Kevin',
+  lastName: 'Van Ryckegem'
+});
 ```
 
 ### insert(table, data)
+
 Note, by default, if data exists with the same keys, the data will be updated!
 
 ```js
-const result = await db.insert('my-db-table', {firstName: 'Kevin', lastName: 'Van Ryckegem'});
+const result = await db.insert('my-db-table', {
+  firstName: 'Kevin',
+  lastName: 'Table'
+});
 ```
 
 ## Roadmap
 
-See the [open issues](https://github.com/kevinvr/dynamongo/issues) for a list of proposed features (and known issues).
-
+See the [open issues](https://github.com/EnsoRings/dynamo-client/issues) for a
+list of proposed features (and known issues).
 
 ## Contributing
 
-Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+Contributions are what make the open source community such an amazing place to
+be learn, inspire, and create. Any contributions you make are
+**greatly appreciated**.
 
 1. Fork the Project
 2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
@@ -107,34 +97,7 @@ Contributions are what make the open source community such an amazing place to b
 4. Push to the Branch (`git push origin feature/AmazingFeature`)
 5. Open a Pull Request
 
-
-
-<!-- LICENSE -->
 ## License
 
 Distributed under the Apache 2.0 License. See `LICENSE` for more information.
 
-<!-- CONTACT -->
-## Contact
-
-Kevin Van Ryckegem - [@kevinvanryck](https://twitter.com/kevinvanryck)
-
-Project Link: [https://github.com/kevinvr/dynamongo](https://github.com/kevinvr/dynamongo)
-
-## Acknowledgements
-Reserved for contributors!
-
-
-<!-- MARKDOWN LINKS & IMAGES -->
-[contributors-shield]: https://img.shields.io/github/contributors/kevinvr/Dynamongo.svg?style=for-the-badge
-[contributors-url]: https://github.com/kevinvr/Dynamongo/graphs/contributors
-[forks-shield]: https://img.shields.io/github/forks/kevinvr/Dynamongo.svg?style=for-the-badge
-[forks-url]: https://github.com/kevinvr/Dynamongo/network/members
-[stars-shield]: https://img.shields.io/github/stars/kevinvr/Dynamongo.svg?style=for-the-badge
-[stars-url]: https://github.com/kevinvr/Dynamongo/stargazers
-[issues-shield]: https://img.shields.io/github/issues/kevinvr/Dynamongo.svg?style=for-the-badge
-[issues-url]: https://github.com/kevinvr/Dynamongo/issues
-[license-shield]: https://img.shields.io/github/license/kevinvr/Dynamongo.svg?style=for-the-badge
-[license-url]: https://github.com/kevinvr/Dynamongo/blob/master/LICENSE.txt
-[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
-[linkedin-url]: https://linkedin.com/in/kevin-van-ryckegem
